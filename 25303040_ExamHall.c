7461557799c6d67685db15e3a0dfba4d4486373a
Md. Irfan Khan <learn.irfankhan@gmail.com>
2026-05-22 06:22:14 +0600

    Commited

--- 25303040_ExamHall.c
+++ 25303040_ExamHall.c
@@ -1,137 +1,220 @@
 #include <stdio.h>
 //-------Task 2: Automatic Seat Allocation
-void allocateSeats(int hall[][10], int hallCount, int seatCount, int studentCount){
-    int firstStudentId=25303001;
+void allocateSeats(int hall[][10], int hallCount, int seatCount, int studentCount)
+{
+    int firstStudentId = 25303001;
     int allocated = 0;
-    for(int i=0;i<hallCount;i++){
-        for(int j=0;j<seatCount;j++){
-            if(allocated<studentCount){
-               hall[i][j]=firstStudentId+1;
+    for (int hallNumber = 0; hallNumber < hallCount; hallNumber++)
+    {
+        for (int seatNumber = 0; seatNumber < seatCount; seatNumber++)
+        {
+            if (allocated < studentCount)
+            {
+
+                hall[hallNumber][seatNumber] = firstStudentId;
+                firstStudentId++;
             }
-            else{
-                hall[i][j]=0;
+            else
+            {
+                hall[hallNumber][seatNumber] = 0;
             }
-            
+
             allocated++;
-
         }
-
     }
 }
 //-------Task 3: Hall Occupancy Analysis
-void hallOccupancy(int hall[][10], int hallCount, int seatCount){
-    int hallNumber,seatNumber,occupied=0,vacant;
+void hallOccupancy(int hall[][10], int hallCount, int seatCount)
+{
+    int hallNumber, seatNumber, occupied = 0, vacant;
     float percentage;
-    for(hallNumber=0;hallNumber<hallCount;hallNumber++){
-        occupied=0;
-        for(seatNumber=0;seatNumber<seatCount;seatNumber++){
-            if(hall[hallNumber][seatNumber]!=0){
+    printf("  \nOccupied seats...\n");
+    for (hallNumber = 0; hallNumber < hallCount; hallNumber++)
+    {
+        occupied = 0;
+        for (seatNumber = 0; seatNumber < seatCount; seatNumber++)
+        {
+            if (hall[hallNumber][seatNumber] != 0)
+            {
                 occupied++;
             }
         }
-        vacant=seatCount-occupied;
-        percentage=((float)occupied/seatCount)*100;
-        printf("Hall-%d:Occopied seat percentage: %.2f percent and vacant seat is %d \n",hallNumber+1,percentage,vacant);
+        vacant = seatCount - occupied;
+        percentage = ((float)occupied / seatCount) * 100;
+        printf("Hall-%d:Occopied seat percentage: %.2f percent and vacant seat is %d \n", hallNumber + 1, percentage, vacant);
     }
-
 }
-
-void overloadedHall(int hall[][10], int hallCount, int seatCount){
-    int hallNumber,seatNumber,occupied=0;
+//--------Task 4: Detect Overloaded Halls
+void detectOverloaded(int hall[][10], int hallCount, int seatCount)
+{
+    int hallNumber, seatNumber, occupied = 0;
     float percentage;
-    for(hallNumber=0;hallNumber<hallCount;hallNumber++){
-        occupied=0;
-        for(seatNumber=0;seatNumber<seatCount;seatNumber++){
-            if(hall[hallNumber][seatNumber]!=0){
+    printf("  \nOverloaded hall...\n");
+    for (hallNumber = 0; hallNumber < hallCount; hallNumber++)
+    {
+        occupied = 0;
+        for (seatNumber = 0; seatNumber < seatCount; seatNumber++)
+        {
+            if (hall[hallNumber][seatNumber] != 0)
+            {
                 occupied++;
             }
         }
-        percentage=((float)occupied/seatCount)*100;
-        if(percentage>=90){
-            printf("Warning:Hall-%d exceeds over 90 percent and Occopied seat percentage: %.2f\n",hallNumber+1,percentage);
-
+        percentage = ((float)occupied / seatCount) * 100;
+        if (percentage >= 90)
+        {
+            printf("Warning:Hall-%d exceeds over 90 percent and Occopied seat percentage: %.2f\n", hallNumber + 1, percentage);
         }
-        
     }
-
 }
-
-void findEmptySeat(int hall[][10], int hallCount, int seatCount){
-    int hallNumber,seatNumber,empty=0;
-    for(hallNumber=0;hallNumber<hallCount;hallNumber++){
-        empty=0;
-        for(seatNumber=0;seatNumber<seatCount;seatNumber++){
-            if(hall[hallNumber][seatNumber]==0){
+//-----------Task 5: Empty Seat Finder
+void findEmptySeat(int hall[][10], int hallCount, int seatCount)
+{
+    int hallNumber, seatNumber, empty = 0;
+    printf("  \nEmpty seats...\n");
+    for (hallNumber = 0; hallNumber < hallCount; hallNumber++)
+    {
+        empty = 0;
+        for (seatNumber = 0; seatNumber< seatCount; seatNumber++)
+        {
+            if (hall[hallNumber][seatNumber] == 0)
+            {
                 empty++;
-                printf("Hall %d: Empty seat postion %d \n",hallNumber+1,seatNumber+1);
+                printf("Hall %d: Empty seat postion %d \n", hallNumber + 1, seatNumber+ 1);
             }
         }
-        
+    }
+}
+//----------Task 6: Student Search Module
+void searchStudent(int hall[][10], int hallCount, int seatCount, int studentID)
+{
+    int hallNumber, seatNumber, f = 0;
+    for (hallNumber = 0; hallNumber < hallCount; hallNumber++)
+    {
+
+        for (seatNumber = 0; seatNumber< seatCount; seatNumber++)
+        {
+            if (hall[hallNumber][seatNumber] == studentID)
+            {
+                f = 1;
+                printf("Hall %d: ID:%d seat postion is %d \n", hallNumber + 1, studentID, seatNumber + 1);
+                break;
+            }
+        }
+    }
+    if (f == 0)
+    {
+        printf("Not Found..");
+    }
+}
+//---------Task 7: Seating Pattern Display
+void displaySeatArrangements(int hall[][10], int hallCount, int seatCount)
+{
+    int hallNumber, seatNumber;
+    printf("  \nSeat Arrangements...\n");
+    for (hallNumber = 0; hallNumber < hallCount; hallNumber++)
+    {
+        printf("\nHall %d:\n", hallNumber+ 1);
+        for (seatNumber = 0; seatNumber < seatCount; seatNumber++)
+        {
+            if (hall[hallNumber][seatNumber] != 0)
+            {
+                printf("%d ", hall[hallNumber][seatNumber]);
+            }
+            else
+            {
+                printf("X ");
+            }
+        }
+        printf("\n");
     }
 }
 
-void searchStudent(int hall[][30], int hallCount, int
-seatCount, int studentID){
-    int hallNumber,seatNumber,f=0;
-    for(hallNumber=0;hallNumber<hallCount;hallNumber++){
-        
-        for(seatNumber=0;seatNumber<seatCount;seatNumber++){
-            if(hall[hallNumber][seatNumber]==studentID){
-                f=1;
-                break;
-            }
-            else if(hall[hallNumber][seatNumber]==0){
-                f=0;
-                break;
+void suggestOptimization(int hall[][10], int hallCount, int seatCount)
+{
+    int hallNumber, seatNumber, occupied = 0, vacant;
+    float percentage;
+    printf("  \nOptimization suggestion...\n");
+    for (hallNumber = 0; hallNumber < hallCount; hallNumber++)
+    {
+        occupied = 0;
+        for (seatNumber= 0; seatNumber < seatCount; seatNumber++)
+        {
+            if (hall[hallNumber][seatNumber] != 0)
+            {
+                occupied++;
             }
         }
-        
+        vacant = seatCount - occupied;
+        percentage = ((float)occupied / seatCount) * 100;
+        if(percentage<25){
+            printf("Hall %d occupide percentage is %.2f percent,Merge the hall with another hall.\n",hallNumber+1,percentage);
+        }
     }
-    (f==1)?printf("Hall %d: ID:%d seat postion is %d \n",hallNumber+1,studentID,seatNumber+1):printf("Not found student");
+    if(percentage>25){
+        printf("No optimization suggestion.\n");
+    }
+}
+
+void reportGenerate(int hall[][10], int hallCount, int seatCount,int studentCount){
+    int hallNumber, seatNumber, occupied = 0, vacant;
+    float percentage;
+    printf("  \n...Overall report...\n");
+    printf("Total hall:%d\n",hallCount);
+    printf("Total student:%d\n",studentCount);
+    displaySeatArrangements(hall,hallCount,seatCount);
+    findEmptySeat(hall,hallCount,seatCount);
+    detectOverloaded(hall,hallCount,seatCount);
+    suggestOptimization(hall,hallCount,seatCount);
 
 }
-int main(){
-    int hallCount=0,seatCount, studentCount,maxSeat;
+int main()
+{
+    int hallCount = 0, seatCount, studentCount, maxSeat, studentId;
     /*This array represents row as exam hall and column as students seat
     Maximum 3 hall and maximum 10 seats per hall*/
     int hallArray[3][10];
 
     //-------Task 1: Input &amp; Validation Module
 
-    //Take number of hall.
+    // Take number of hall.
     printf("Enter number of hall(1-3):");
-    while(hallCount>3 || hallCount<=0){
-        scanf("%d",&hallCount);
-        if(hallCount>3 || hallCount<=0){
+    while (hallCount > 3 || hallCount <= 0)
+    {
+        scanf("%d", &hallCount);
+        if (hallCount > 3 || hallCount <= 0)
+        {
             printf("Enter again:");
         }
     }
-    //Take input number of seats.
+    // Take input number of seats.
     printf("Enter number of seats per hall:");
-    while(seatCount>10 || seatCount<=0){
-        scanf("%d",&seatCount);
-        if(seatCount>10 || seatCount<=0){
+    while (seatCount > 10 || seatCount <= 0)
+    {
+        scanf("%d", &seatCount);
+        if (seatCount > 10 || seatCount <= 0)
+        {
             printf("Enter again:");
         }
     }
-     //Calculates maximum seat.
-    maxSeat=seatCount*hallCount;
-    //Take input number of students.
-    printf("Enter students(1- %d):",maxSeat);
-    while(studentCount>maxSeat || studentCount<=0){
-        scanf("%d",&studentCount);
-        if(studentCount>maxSeat || studentCount<=0){
+    // Calculates maximum seat.
+    maxSeat = seatCount * hallCount;
+    // Take input number of students.
+    printf("Enter students(1- %d):", maxSeat);
+    while (studentCount > maxSeat || studentCount <= 0)
+    {
+        scanf("%d", &studentCount);
+        if (studentCount > maxSeat || studentCount <= 0)
+        {
             printf("Enter again:");
         }
     }
-   
-    //This function used to allocate seats.
-    allocateSeats(hallArray,hallCount,seatCount,studentCount);
-    //This function used to callculate occupancy
-    hallOccupancy(hallArray, hallCount, seatCount);
 
-    overloadedHall(hallArray, hallCount, seatCount);
-
-    findEmptySeat(hallArray, hallCount,seatCount);
-
+    // This function used to allocate seats.
+    allocateSeats(hallArray, hallCount, seatCount, studentCount);
+    reportGenerate(hallArray,hallCount,seatCount,studentCount);
+    printf("Enter Student Id to search student:");
+    scanf("%d", &studentId);
+    searchStudent(hallArray, hallCount, seatCount, studentId);
+    
 }
